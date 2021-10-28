36524afe5fb0b511862196b66bd87c325ee7d863
Chabla <chabijeanbaptiste@gmail.com>
2021-10-28 13:36:21 +0000

    all modification

--- app/models/prestation.rb
+++ app/models/prestation.rb
@@ -1,6 +1,7 @@
 class Prestation < ApplicationRecord
   belongs_to :hospital
   has_many :payment, dependent: :destroy
+  has_many :comment, dependent: :destroy
   validates :name_prest, presence:true
   validates :price, presence:true
 end
